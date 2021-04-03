a1568579e81cbec1f1ca4f2a74a43ab469592382
John Bozarov <usaboz@gmail.com>
2021-04-03 16:20:02 -0500

    Overriding property name using @JsonProperty annotation

--- mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -25,7 +25,7 @@ public class BeerDtoKebabTest extends BaseTest {
     @Test
     void testDeserializeKebabCase() throws IOException {
 
-        String jsonKebab = "{\"id\":\"0da5d8e4-f345-4a3d-a57f-1806815aeb0a\",\"beer-name\":\"BeerName\",\"beer-style\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"created-date\":\"2021-04-03T16:08:12.3896373-05:00\",\"last-updated-date\":\"2021-04-03T16:08:12.390642-05:00\"}\n";
+        String jsonKebab = "{\"beerId\":\"0da5d8e4-f345-4a3d-a57f-1806815aeb0a\",\"beer-name\":\"BeerName\",\"beer-style\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"created-date\":\"2021-04-03T16:08:12.3896373-05:00\",\"last-updated-date\":\"2021-04-03T16:08:12.390642-05:00\"}\n";
 
         BeerDto dto = objectMapper.readValue(jsonKebab, BeerDto.class);
 
