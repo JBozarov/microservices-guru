137e2a8e38b72ae6180a1e6c8362987234aebad8
John Bozarov <usaboz@gmail.com>
2021-04-03 16:14:20 -0500

    Code optimized, Ojbectmapper moved to BaseTest

--- mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -2,9 +2,7 @@ package guru.springframework.msscjacksonexamples.model;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -13,9 +11,6 @@ import java.io.IOException;
 @ActiveProfiles("kebab")
 @JsonTest
 public class BeerDtoKebabTest extends BaseTest {
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     @Test
     void testKebabCase () throws JsonProcessingException {
