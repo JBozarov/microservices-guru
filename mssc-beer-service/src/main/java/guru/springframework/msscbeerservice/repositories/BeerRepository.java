92e3c89269c2a2a2480c8c0f0efecb95b06cbf9d
John Bozarov <usaboz@gmail.com>
2021-04-06 17:20:40 -0500

    2 microservices are communicating

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -3,9 +3,17 @@ package guru.springframework.msscbeerservice.repositories;
 import guru.springframework.msscbeerservice.domain.Beer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 @Repository
 public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
 }
--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/service/BeerService.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/service/BeerService.java
@@ -2,10 +2,16 @@ package guru.springframework.msscbeerservice.service;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.ResponseEntity;
+import guru.springframework.msscbeerservice.web.model.BeerPagedList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.UUID;
 
 public interface BeerService {
+
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
+
     BeerDto getBeerById(UUID beerId);
 
     BeerDto saveNewBeer(BeerDto beerDto);
