ef651ac1c214633925efeb08761bf9ea7f57bf7a
John Bozarov <usaboz@gmail.com>
2021-04-20 14:59:38 -0500

    208 Initial state from db

--- mssc-ssm/src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -1,11 +1,13 @@
 package guru.springframework.msscssm.services;
 
 import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
 import guru.springframework.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 import java.awt.event.PaintEvent;
@@ -15,7 +17,7 @@ import java.awt.event.PaintEvent;
 public class PaymentServiceImpl implements PaymentService {
 
   private final PaymentRepository paymentRepository;
-  private final StateMachineFactory<PaymentState, PaintEvent> stateMachineFactory;
+  private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
 
   @Override
   public Payment newPayment(Payment payment) {
@@ -24,17 +26,37 @@ public class PaymentServiceImpl implements PaymentService {
   }
 
   @Override
-  public StateMachine<PaymentState, PaintEvent> preAuth(Long paymentId) {
+  public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+    StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
     return null;
   }
 
   @Override
-  public StateMachine<PaymentState, PaintEvent> authorizePayment(Long paymentId) {
+  public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+    StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
     return null;
   }
 
   @Override
-  public StateMachine<PaymentState, PaintEvent> declineAuth(Long paymentId) {
+  public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+    StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
     return null;
+  }
+
+  private StateMachine<PaymentState, PaymentEvent> build(Long paymentId){
+    Payment payment = paymentRepository.getOne(paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+    sm.stop();
+
+    sm.getStateMachineAccessor()
+            .doWithAllRegions(sma -> {
+              sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+            });
+
+    sm.start();
+
+    return sm;
   }
 }
