dc7a76bb173b6bdada354581bac1eef57786d00f
John Bozarov <usaboz@gmail.com>
2021-04-21 14:51:59 -0500

    transition configured

--- side-projects/state-machine/src/main/java/com/statemachine/SMConfig.java
+++ side-projects/state-machine/src/main/java/com/statemachine/SMConfig.java
@@ -6,6 +6,7 @@ import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
@@ -16,27 +17,41 @@ import java.util.EnumSet;
 @EnableStateMachineFactory
 public class SMConfig extends StateMachineConfigurerAdapter<OrderStates, OrderEvents> {
 
+  @Override
+  public void configure(StateMachineConfigurationConfigurer<OrderStates, OrderEvents> config)
+      throws Exception {
 
-    @Override
-    public void configure(StateMachineConfigurationConfigurer<OrderStates, OrderEvents> config) throws Exception {
-        StateMachineListenerAdapter<OrderStates, OrderEvents> adapter = new StateMachineListenerAdapter<OrderStates, OrderEvents>() {
-            @Override
-            public void stateChanged(State<OrderStates, OrderEvents> from, State<OrderStates, OrderEvents> to) {
-                log.debug(" from: " + from.toString() + " and to: " + to.toString() );
-            }
+    StateMachineListenerAdapter<OrderStates, OrderEvents> adapter =
+        new StateMachineListenerAdapter<OrderStates, OrderEvents>() {
+          @Override
+          public void stateChanged(
+              State<OrderStates, OrderEvents> from, State<OrderStates, OrderEvents> to) {
+            log.debug(" from: " + from.toString() + " and to: " + to.toString());
+          }
         };
-        config.withConfiguration()
-                .autoStartup(false)
-                .listener(adapter);
-    }
+
+    config.withConfiguration().autoStartup(false).listener(adapter);
+  }
+
+  @Override
+  public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> states)
+      throws Exception {
+    states
+        .withStates()
+        .initial(OrderStates.SUBMITTED)
+        .state(OrderStates.PAID)
+        .end(OrderStates.CANCELLED)
+        .end(OrderStates.FULFILLED);
+  }
 
     @Override
-    public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> states) throws Exception {
-        states
-                .withStates()
-                .initial(OrderStates.SUBMITTED)
-                .states(EnumSet.allOf(OrderStates.class))
-                .end(OrderStates.CANCELLED)
-                .end(OrderStates.FULFILLED);
+    public void configure(StateMachineTransitionConfigurer<OrderStates, OrderEvents> transitions) throws Exception {
+        transitions
+                .withExternal()
+                .source(OrderStates.SUBMITTED).target(OrderStates.PAID).event(OrderEvents.PAY)
+                .and()
+                .withExternal()
+                .source(OrderStates.PAID).target(OrderStates.FULFILLED).event(OrderEvents.FULFILL);
+
     }
 }
